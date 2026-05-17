0d8c8978c0d3272138a419645d584720b6de39bb
Hardiing <nathanharding02@gmail.com>
2026-05-17 02:32:09 -0600

    Ch 1.5 - help/exit REPL

--- main.go
+++ main.go
@@ -1,18 +1,11 @@
 package main
 
-import (
+/* import (
 	"bufio"
 	"fmt"
 	"os"
-)
+) */
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		input := scanner.Text()
-		r := cleanInput((input))
-		fmt.Printf("Your command was: %s\n", r[0])
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,8 +1,17 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
 
 func cleanInput(text string) []string {
 	s := []string{}
@@ -12,4 +21,51 @@ func cleanInput(text string) []string {
 		s = append(s, lowerStr)
 	}
 	return s
+}
+
+func startRepl() {
+	var supportedCommands map[string]cliCommand
+
+	commandHelp := func() error {
+		fmt.Println("Welcome to the Pokedex!")
+		fmt.Println("Usage:")
+		fmt.Println("")
+		for _, command := range supportedCommands {
+			fmt.Printf("%s: %s\n", command.name, command.description)
+		}
+		return nil
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		r := cleanInput((input))
+		supportedCommands = map[string]cliCommand{
+			"exit": {
+				name:        "exit",
+				description: "Exit the Pokedex",
+				callback:    commandExit,
+			},
+			"help": {
+				name:        "help",
+				description: "Displays a help message",
+				callback:    commandHelp,
+			},
+		}
+		c, ok := supportedCommands[r[0]]
+		if ok {
+			c.callback()
+		} else {
+			fmt.Println("Unknown command")
+		}
+
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
 }
